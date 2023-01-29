eeafb0679fbed5bada80be9c3c41236d1fe5a4f2
Dimitar Chetelev <chetelev@gmail.com>
2023-01-29 16:59:01 +0100

    init

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -8,7 +8,6 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
-import e from 'express';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 import { Task, TaskStatus } from './task.model';
